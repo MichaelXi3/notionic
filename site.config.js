2011baae74fdf949d0505a7a8c6251dfd9d7fac4
Michael Xi <michaelxi@MacBookPro.lan>
2024-10-26 18:30:06 -0400

    change config

--- site.config.js
+++ site.config.js
@@ -25,11 +25,11 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://notionic-five-delta.vercel.app/",
-  since: "", // If leave this empty, current year will be used.
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ["Blog", "WAFL", "File System", "Yukun Xi", "Spherex", "Michael Xi"],
+    keywords: ["Blog", "WAFL", "File System", "Yukun Xi", "void", "Michael Xi"],
   },
 
   // notion configuration (required)
